f61faec46a368c7fbc56bdda52c9edf0f8388d05
Alfredo Palacios <joseaph.1998@gmail.com>
2026-07-03 08:22:10 -0600

    Setup Firebase credentials file in AppServiceProvider during boot.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $credentialsJson = env('FIREBASE_CREDENTIALS_JSON');
+        $credentialsPath = env('FIREBASE_CREDENTIALS');
+
+        if ($credentialsJson && $credentialsPath) {
+            File::ensureDirectoryExists(dirname($credentialsPath));
+
+            file_put_contents($credentialsPath, $credentialsJson);
+        }
     }
 }
